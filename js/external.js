39902477be440e36661fa7c2ac30aa6acc6a7c99
Casanova Geary <CasanovaGeary@Gmail.com>
2022-07-28 16:54:15 -0500

    External JS exercise

--- js/external.js
+++ js/external.js
@@ -10,17 +10,15 @@ like it). If price for a movie per day is $3,
 how much will you have to pay?
 */
 
-var LittleMermaid = 3;
-var LittleMermaid = prompt("How many days have you rented this movie?");
-var BrotherBear = 5;
-var BrotherBear = prompt("How many days have you rented this movie?");
-var Hercules = 1;
-var Hercules = prompt("How many days have you rented this movie?");
 
-var day = 9;
-var price = 3;
-var total = day * price;
-alert("Your current total price for movie rentals will be $ .");
+var LittleMermaid = prompt("How many days have you rented Little Mermaid?");
+var BrotherBear = prompt("How many days have you Brother Bear?");
+var Hercules = prompt("How many days have you rented Hercules?");
+var costLM  = LittleMermaid * 3;
+var costBB = BrotherBear * 3;
+var costH = Hercules * 3;
+
+alert("Your current total price is " + "$ " + (costLM+costBB+costH) + " .");
 
 /*Tried calculating tally for days using shorthand assignment operators,
 becasue the movies have not been returned (don't know yet if kids will
@@ -33,38 +31,57 @@ Amazon $380, and Facebook $350. How much will you receive in payment for
 this week? You worked 10 hours for Facebook, 6 hours for Google and 4 hours
 for Amazon. */
 
+
 var FB = 350;
-var FB = prompt("How much does Facebook pay contractors per hour?");
+
 var Amazon = 380;
-var Amazon = prompt("How much does Amazon pay contractors per hour?");
+
 var Google = 400;
-var Google = prompt("How much does Google pay contractors per hour?");
-var Fh = 10;
+
 var Fh = prompt("How many hours did you work for Facebook?");
-var Ah = 4;
+
 var Ah = prompt("How many hours did you work for Amazon?");
-var Gh = 6;
+
 var Gh = prompt("How many hours did you work for Facebook?");
+
 var totalFB = FB * Fh;
-var totalAmazon = Amazon* Ah;
-var totalGoogle = Google * Gh;
-totalGoogle + totalFB + totalAmazon; //$7420//
-alert("Your total payment received for this week is $ .");
+var totalAMZN = Amazon * Ah;
+var totalGgle = Google * Gh;
+
+alert("Your total payment received for this week is " + "$" + (totalFB+totalAMZN+totalGgle) + " .");
 
 /*A student can be enrolled in a class only if the class is not full
 and the class schedule does not conflict with her current schedule.
 */
 
-var isFull = false;
-var isConflict = false;
-let enroll = (!isFull && !isConflict);
+var isFull = confirm("Is the class full?");
+var isConflict = confirm("Does the class schedule conflict with current schedule?")
+var confirmConflict = !isFull && !isConflict;
+alert("Enrollment status: " + confirmConflict);
+
+/*var isFull = confirm("Is the class full?");
+var isConflict = confirm("Does the class schedule conflict with current schedule?");
+var confirm = !isFull && !isConflict;
+alert("Approved");*/
+
+/* A product offer can be applied only if a person buys more than 2 items,
+and the offer has not expired. Premium members do not need to buy a
+specific amount of products.
+*/
+
+
+/*var purchased = prompt("Quantity");
+var product = true > 2;
+var expired = false;
+var premium = prompt("Premium Member?");
+var confirm = purchased && expired;
+var confirmPremium = !premium;
+alert("Product offer" + confirmPremium + confirm);*/
+
+var product = confirm("Purchased more than two?");
+var expired = confirm("Offer expired?");
+var confirmProduct = (product && !expired);
+alert("Status: " + confirmProduct);
 
 
 
-/*Exercises
-Complete exercise 3 from the previous lesson, but write your code in the external.js
-file instead of in the console.
-
-When the exercise asks you to use a number, instead use a prompt to ask the user for that number.
-Use an alert to show the results of each problem.
-Finally, commit the changes to your git repository, and push to GitHub.*/
