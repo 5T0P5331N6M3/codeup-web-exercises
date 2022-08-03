1ed842a8d0826b072cb1d9571764445d1f09a164
Casanova Geary <CasanovaGeary@Gmail.com>
2022-08-03 16:18:43 -0500

    Iterating Exercise

--- js/for_loops.js
+++ js/for_loops.js
@@ -9,5 +9,49 @@
 //  2. Create a function named showMultiplicationTable accepting a number and
 //      console.logs the multiplication table for that number (just multiply by the numbers 1-10.
 function showMultiplicationTable(x) {
+    for ( ; ; ;) {
+
+    }
     console.log(showMultiplicationTable);
 }
+//  3. Use a for loop and the code from the previous lessons to generate 10 random numbers between 20 and 200,
+//      and output to the console whether each number is odd or even.
+//  For example:
+// 123 is odd
+// 80 is even
+// 24 is even
+// 199 is odd
+// ...
+
+//  4. Create a for loop using console.log to create the output shown below
+// 1
+// 22
+// 333
+// 4444
+// 55555
+// 666666
+// 7777777
+// 88888888
+// 999999999
+
+//  5. Create a for loop using console.log to create output shown below.
+//100
+// 95
+// 90
+// 85
+// 80
+// 75
+// 70
+// 65
+// 60
+// 55
+// 50
+// 45
+// 40
+// 35
+// 30
+// 25
+// 20
+// 15
+// 10
+// 5
