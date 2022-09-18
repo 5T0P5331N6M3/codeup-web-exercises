cc93eac6f935e102fecc376e675194df20dd1fcc
Casanova Geary <CasanovaGeary@Gmail.com>
2022-09-17 19:19:37 -0500

    for_loops js Completed.

--- js/for_loops.js
+++ js/for_loops.js
@@ -9,7 +9,6 @@
 //  2. Create a function named showMultiplicationTable accepting a number and
 //      console.logs the multiplication table for that number (just multiply by the numbers 1-10.
 function showMultiplicationTable(num) {
-
     for (let i = 1; i <= 10; i++) {
         console.log(num * i);
     }
@@ -24,8 +23,25 @@ showMultiplicationTable(7)
 // 80 is even
 // 24 is even
 // 199 is odd
-// ...
+const randomNumber = Math.floor(Math.random() * 10) + 1;
 
+function randomLoop(num) {
+    for (let i = 20; i <= 200; i++) {
+        console.log(randomNumber);
+    }
+    if (number % 2 === 0) {
+        console.log('Number ' + randomNumber + ' is even.');
+
+    } else {
+        console.log('Number ' + randomNumber + ' is odd.');
+    }
+}
+// How do I check in console if this works?
+
+// This is how you get a random number between 50 and 100
+var allCones = Math.floor(Math.random() * 50) + 50;
+// This expression will generate a random number between 1 and 5
+Math.floor(Math.random() * 5) + 1;
 
 //  4. Create a for loop using console.log to create the output shown below
 // 1
@@ -37,8 +53,8 @@ showMultiplicationTable(7)
 // 7777777
 // 88888888
 // 999999999
-
-for (let i = 1; i <= 9; i ++) {
+// Why does "toString" work here? Nothing in code suggests a string when console logging.
+for (let i = 1; i <= 9; i++) {
     console.log(i.toString().repeat(i));
 }
 // console.log()
@@ -64,3 +80,7 @@ for (let i = 1; i <= 9; i ++) {
 // 15
 // 10
 // 5
+
+for (let i = 100; i >= 5; i-=5) {
+    console.log(i);
+}
