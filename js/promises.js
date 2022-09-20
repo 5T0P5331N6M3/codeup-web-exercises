d431ecec16e0f7ad590f887351e4fd0465655cbf
Casanova Geary <CasanovaGeary@Gmail.com>
2022-09-19 20:55:01 -0500

    Need to read more how/what to use for API.

--- js/promises.js
+++ js/promises.js
@@ -1,2 +1,19 @@
 // fetch(url, {headers: {'Authorization': 'GITHUB_TOKEN'}})
 
+//  TODO: Create a function accepting a GitHub username, and returns a promise resolving returning just the date of the last commit the user made. Reference the github api documentations to achieve this.
+
+//  Accepts GitHub username
+// Octokit.js
+// https://github.com/octokit/core.js#readme
+const octokit = new Octokit({
+    auth: 'GITHUB_TOKEN'
+})
+
+await octokit.request('GET /users/{username}/events/public', {
+    username: 'USERNAME'
+})
+
+const lastCommitDate = fetch('https://api.github.com/users')
+    .then(response => console.log(response))
+    .catch(error => console.error(error));
+//  Returns a promise resolving returning just the date of last commit user made.
