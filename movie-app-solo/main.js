a6a6d08790b89d60599c26e3ac0d14c2190d5859
Casanova Geary <CasanovaGeary@Gmail.com>
2022-10-01 20:26:46 -0500

    redoing everything.

--- movie-app-solo/main.js
+++ movie-app-solo/main.js
@@ -1,17 +1,17 @@
 'use strict';
 
 /* LOADER */
-document.onreadystatechange = function () {
-    if (document.readyState !== "complete") {
-        document.querySelector("body").style.visibility = "hidden";
-        document.querySelector("#loader").style.visibility = "visible";
-    } else {
-        setTimeout(() => {
-            document.querySelector("#loader").style.display = "none";
-            document.querySelector("body").style.visibility = "visible";}, 1000);
-
-    }
-};
+// document.onreadystatechange = function () {
+//     if (document.readyState !== "complete") {
+//         document.querySelector("body").style.visibility = "hidden";
+//         document.querySelector("#loader").style.visibility = "visible";
+//     } else {
+//         setTimeout(() => {
+//             document.querySelector("#loader").style.display = "none";
+//             document.querySelector("body").style.visibility = "visible";}, 1000);
+//
+//     }
+// };
 
 /* FETCH GLITCH API */
 // function fetchFavoriteMovies() {
@@ -53,3 +53,43 @@ document.onreadystatechange = function () {
 //         });
 // }
 // fetchFavoriteMovies();
+
+// function fetchFavoriteMovies() {
+//     fetch('http://www.omdbapi.com/?apikey=${OMDB_KEY}&\n')
+//          .then(response => {
+//              console.log(response);
+//              // const data = response.json();
+//             if (!response.ok) {
+//                  throw Error('ERROR');
+//              }
+//              return response.json();
+//          })
+//          .then(data => {
+//              console.log(data);
+//              const favoriteHTML = data.map(favMovie => {
+//                  return `
+// //                         <!-- Movie Card -->
+//                      <div id="favCard" class="card" style="margin: 4rem;">
+//                          <div class="fav-card-content">
+//                          <h5 class="fav-title">${favMovie.title}</h5>
+//                              <div class="fav-body">
+//                                  <img class="fav-poster" src="${favMovie.poster}" alt="Movie Title" style="width: 250px; height: 30vh;">
+//                                  <p class="fav-director">Director: ${favMovie.director}</p>
+//                                  <p class="fav-rated">Rated: ${favMovie.rated}</p>
+//                                  <p class="fav-genre">Genre: ${favMovie.genre}</p>
+//                                   <p class="fav-rating">Rating: ${favMovie.rating}</p>
+//                              </div>
+//                              <div class=" card-footer fav-footer">
+//                                  <button type="button" id="edit">Edit</button>
+//                                  <button type="button" id="delete">Delete</button>
+//                              </div>
+//                          </div>
+//                      </div>
+//                  `;
+//              })
+//                  .join("");
+//              // console.log(html);
+//              $('#favoriteWatchlist').html(favoriteHTML);
+//          });
+//  }
+//  fetchFavoriteMovies();
